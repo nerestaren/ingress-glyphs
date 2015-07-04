6364f735250773befcec0c529fb487a225966e87
Antoni Oliver <antoni.oliver.tomas@gmail.com>
2015-07-04 14:35:49 +0200

    more glyphs, IITC draw, glyph names, infowindow, paths
    glyphs: A-C done.

--- glyphs.js
+++ glyphs.js
@@ -1,8 +1,52 @@
+//          10
+//  5               9
+//      4       1
+//          0
+//      3       2
+//  6               8
+//          7
+
+
+/*
+    To add more glyphs, go to glyphtionary.com and execute these commands in javascript console ;)
+    http://glyphtionary.com/
+    $('.glyph-cell').css('position', 'relative');
+    $('.glyph-cell').prepend($('<img>').prop('src', 'http://nerestaren.github.io/ingress-glyphs/img/reference.png').css('position', 'absolute'))
+*/
+
 var glyphs = {
 	// A
-	avoid: [5, 10, 1, 9, 2],	
+    abandon: [9, 1, 0, 3, 6, 7],
+    adapt: [5, 3, 0, 2],
+    advance: [10, 4, 6],
+    after: [9, 1, 0, 2, 8, 9],
+    again_repeat: [6, 4, 3, 0, 1, 2],
+    all: [10, 9, 8, 7, 6, 5, 10],
+    answer: [4, 1, 2, 0],
+    attack_war: [6, 4, 10, 1, 8],
+	avoid_struggle: [5, 10, 1, 9, 2],
+    // B
+    barrier_obstacle: [10, 0, 2, 8],
+    before: [5, 4, 0, 3, 6, 5],
+    begin: [10, 3, 7, 2],
+    being_human: [4, 1, 2, 7, 3, 4],
+    body_shell: [4, 1, 0, 4],
+    breathe: [5, 4, 0, 1, 9],
 	// C
 	capture: [7, 6, 0, 2, 9],
+    change_modify: [3, 0, 7, 2],
+    chaos_disorder: [6, 5, 10, 9, 1, 0, 3, 7],
+    clear: [10, 0, 7],
+    clearAll: [10, 0, 7, 8, 9, 10, 5, 6, 7],
+    complex: [1, 4, 0, 3],
+    conflict: [6, 4, 3, 2, 1],
+    consequence: [5, 4, 3, 2, 8],
+    contemplate: [1, 0, 4, 3, 7, 8, 9, 10],
+    contract_reduce: [2, 1, 8],
+    courage: [6, 4, 3, 2],
+    create_creation: [6, 3, 0, 1, 9],
+    creativity: [0, 4, 7, 0],
+    creativity_mind_thought_idea: [3, 6, 5, 4, 0, 2, 8, 9, 1],
 	// D
 	danger: [10, 4, 0, 7],
 	// E
@@ -11,6 +55,8 @@ var glyphs = {
 	future: [9, 1, 2, 8],
 	// G
 	gain: [5, 4],
+    // H
+    help: [5, 4, 0, 3, 2],
 	// L
 	liberate: [6, 4, 0, 9, 10],
 	// P
--- script.js
+++ script.js
@@ -21,12 +21,35 @@ var currentGlyph = undefined;
 var map, currentInfowindow;
 
 function glyphName(a) {
-    var s = a.split('_');
+    /*var s = a.split('_');
     var r = s[0].substr(0, 1).toUpperCase() + s[0].substr(1);;
     for (var i = 1; i < s.length; i++) {
         r += ' - ' + s[i].substr(0, 1).toUpperCase() + s[i].substr(1);
     }
+    return r;*/
+    var r = a.charAt(0).toUpperCase();
+    var i = 1;
+    while (i < a.length) {
+        var c = a.charAt(i);
+        if (c === '_') {
+            i++;
+            r += ' / ' + a.charAt(i).toUpperCase();
+        } else if (c == c.toUpperCase()) {
+            r += ' ' + c.toLowerCase();
+        } else {
+            r += c;
+        }
+        i++;
+    }
     return r;
+}
+
+function splitColor(c) {
+    return [
+        parseInt(c.substr(1, 2), 16),
+        parseInt(c.substr(3, 2), 16),
+        parseInt(c.substr(5, 2), 16)
+    ];
 }
 
 $(document).ready(function() {
@@ -67,10 +90,7 @@ $(document).ready(function() {
     currentGlyph = $('#glyph').val();
     
     // Background color value
-    var t = $('#colorpicker').val();
-    bgcolor[0] = parseInt(t.substr(1, 2), 16);
-    bgcolor[1] = parseInt(t.substr(3, 2), 16);
-    bgcolor[2] = parseInt(t.substr(5, 2), 16);
+    bgcolor = splitColor($('#colorpicker').val());
     
     // Get last used values
     var zoom = null, lat = null, lng = null;
@@ -84,16 +104,14 @@ $(document).ready(function() {
         }
         if (localStorage.getItem('bgcolor') !== null) {
             var t = localStorage.getItem('bgcolor');
-            bgcolor[0] = parseInt(t.substr(1, 2), 16);
-            bgcolor[1] = parseInt(t.substr(3, 2), 16);
-            bgcolor[2] = parseInt(t.substr(5, 2), 16);
+            bgcolor = splitColor(t);
             $('#colorpicker').val(t);
         }
     }
     
     // Draw elements
-    drawBackground();
-    drawGlyph();
+    drawBackground(canvasbg, bgcolor);
+    drawGlyph(canvasg, currentGlyph);
     mix();
         
     // Init map
@@ -102,6 +120,8 @@ $(document).ready(function() {
         center: new google.maps.LatLng(lat !== null ? lat : 39.569411, lng !== null ? lng : 2.650168),
         styles: [{"featureType":"all","elementType":"geometry.fill","stylers":[{"color":"#191f1f"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#a4b0b0"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"weight":"1.5"}]},{"featureType":"administrative.country","elementType":"geometry.fill","stylers":[{"color":"#0c0e0e"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"color":"#9a9a9a"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#191f1f"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#191f1f"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#0c0e0e"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry.fill","stylers":[{"color":"#0c0e0e"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.fill","stylers":[{"color":"#223030"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#38a6a6"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#52c2c2"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.fill","stylers":[{"color":"#38a6a6"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.stroke","stylers":[{"color":"#38a6a6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#080b0b"},{"weight":"0.80"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#3f3f3f"},{"weight":"0.50"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#080b0b"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#2c3434"},{"weight":"0.41"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c6d4ec"}]}]
     });
+    
+    var iitc = [];
     
     for (var i = 0; i < missions.length; i++) {
         var regex = /&pls=(\d*\.\d*,\d*\.\d*,\d*\.\d*,\d*\.\d*(?:_\d*\.\d*,\d*\.\d*,\d*\.\d*,\d*\.\d*)+)/;
@@ -119,23 +139,51 @@ $(document).ready(function() {
             title: glyphName(missions[i].glyph)
         });
         
-        var infowindow = new google.maps.InfoWindow({
-            content: '<h3><b>' + glyphName(missions[i].glyph) + '</b></h3>\
-                <a href="' + missions[i].portals + '" target="_blank">Intel link (portals)</a><br>\
-                <a href="' + missions[i].link + '" target="_blank">Intel link (mission)</a>'
-        });
-        
-        (function (marker, infowindow) {
+        (function (marker, mission) {
             google.maps.event.addListener(marker, 'click', function() {
                 if (currentInfowindow !== undefined) {
                     currentInfowindow.close();
-                    currentInfowindow = undefined;
                 }
-                currentInfowindow = infowindow;
-                infowindow.open(map, marker);
+                currentInfowindow = new google.maps.InfoWindow({
+                    content: '\
+                        <div class="in-infowindow">\
+                            <h3><b>' + glyphName(mission.glyph) + '</b></h3>\
+                            <canvas id="mission-canvas" width="128" height="128"></canvas><br>\
+                            <a href="' + mission.portals + '" target="_blank">Intel link (portals)</a><br>\
+                            <a href="' + mission.link + '" target="_blank">Intel link (mission)</a>\
+                        </div>'
+                });;
+                google.maps.event.addListener(currentInfowindow, 'domready', function () {
+                    drawMissionCanvas(mission);
+                });
+                currentInfowindow.open(map, marker);
             });
-        })(marker, infowindow);
+        })(marker, missions[i]);
         
+        
+        iitc.push({
+            type: 'marker',
+            latLng: {
+                lat: coords[0],
+                lng: coords[1]
+            },
+            color: missions[i].color
+        });
+        
+        iitc.push({
+            type: 'polyline',
+            latLngs: [
+                {
+                    lat: coords[0],
+                    lng: coords[1]
+                },
+                {
+                    lat: coords[2],
+                    lng: coords[3]
+                }
+            ],
+            color: missions[i].color
+        });
         
         for (var l = 0; l < linksRaw.length; l++) {
             var coords = linksRaw[l].split(',');
@@ -152,17 +200,25 @@ $(document).ready(function() {
                 strokeWeight: 2,
                 map: map
             });
-        }
         
-        /*var polyLine = new google.maps.Polyline({
-            path: path,
-            geodesic: true,
-            strokeColor: missions[i].color,
-            strokeOpacity: 1.0,
-            strokeWeight: 2,
-            map: map
-        });*/
+            iitc.push({
+                type: 'polyline',
+                latLngs: [
+                    {
+                        lat: coords[0],
+                        lng: coords[1]
+                    },
+                    {
+                        lat: coords[2],
+                        lng: coords[3]
+                    }
+                ],
+                color: missions[i].color
+            });
+        }
     }
+    
+    $('#iitc').val(JSON.stringify(iitc));
     
     google.maps.event.addListener(map, 'click', function() {
         if (currentInfowindow !== undefined) {
@@ -182,28 +238,29 @@ $(document).ready(function() {
     }
 });
 
-function drawBackground() {
-    ctbg.clearRect(0, 0, 512, 512);
-    ctbg.drawImage(background, 0, 0);
-    var imgdata = ctbg.getImageData(0, 0, 512, 512);
+function drawBackground(canvas, color) {
+    var ct = canvas.getContext('2d');
+    ct.clearRect(0, 0, canvas.width, canvas.height);
+    ct.drawImage(background, 0, 0, canvas.width, canvas.height);
+    var imgdata = ct.getImageData(0, 0, canvas.width, canvas.height);
     var pixels = imgdata.data;
     
     var r, g, b, br, bg, bb;
     for (var i = 0, il = pixels.length; i < il; i += 4) {
-        pixels[i] = hardLight(pixels[i], bgcolor[0]);
-        pixels[i + 1] = hardLight(pixels[i + 1], bgcolor[1]);
-        pixels[i + 2] = hardLight(pixels[i + 2], bgcolor[2]);
+        pixels[i] = hardLight(pixels[i], color[0]);
+        pixels[i + 1] = hardLight(pixels[i + 1], color[1]);
+        pixels[i + 2] = hardLight(pixels[i + 2], color[2]);
     }
     
     
-    ctbg.putImageData(imgdata, 0, 0);
-    ctbg.drawImage(callibrationGrid, 0, 0);
+    ct.putImageData(imgdata, 0, 0);
+    ct.drawImage(callibrationGrid, 0, 0, canvas.width, canvas.height);
 }
 
-function drawGlyph() {
-    if (currentGlyph !== undefined) {
-        ctg.clearRect(0, 0, 512, 512);
-        var path = glyphs[currentGlyph];
+function drawGlyph(canvas, glyph) {
+    if (glyph !== undefined) {
+        var ct = canvas.getContext('2d');
+        var path = glyphs[glyph];
         for (var i = 1; i < path.length; i++) {
             var a = grid[path[i - 1]];
             var x1 = a[0]; var y1 = a[1];
@@ -217,10 +274,24 @@ function drawGlyph() {
                 var t1 = 1 - t;
                 var x = t1 * x1 + t * x2 + Math.random() * 10 - 5;
                 var y = t1 * y1 + t * y2 + Math.random() * 10 - 5;
-                drawPoint(x, y);
+                drawPoint(ct, x, y);
             }
         }
     }
+}
+
+function drawMissionCanvas(mission) {
+    var c = document.getElementById('mission-canvas');
+    var color = splitColor(mission.color)
+    drawBackground(c, color);
+    
+    var aux = document.createElement('canvas');
+    aux.width = '512';
+    aux.height = '512';
+    drawGlyph(aux, mission.glyph);
+    var tmp = new Image();
+    tmp.src = aux.toDataURL("image/png");
+    c.getContext('2d').drawImage(tmp, 0, 0, 512, 512, 0, 0, 128, 128);
 }
 
 function mix() {
@@ -236,22 +307,23 @@ function hardLight(b, a) {
         : (255 - 2 * (255 - a) * (255 - b) / 255);
 }
 
-function drawPoint(x, y) {
-    ctg.beginPath();
-    ctg.arc(x, y, 2.5, 0, Math.PI * 2, true);
-    ctg.closePath();
-    ctg.fillStyle = '#FFFFFF';
-    ctg.shadowColor = 'rgb(221, 163, 121)';
-    ctg.shadowBlur = 16;
-    ctg.shadowOffsetX = 0;
-    ctg.shadowOffsetY = 0;
-    ctg.strokeStyle = 'rgb(255, 209, 176)';
-    ctg.stroke();
-    ctg.fill();
+function drawPoint(ct, x, y) {
+    ct.beginPath();
+    ct.arc(x, y, 2.5, 0, Math.PI * 2, true);
+    ct.closePath();
+    ct.fillStyle = '#FFFFFF';
+    ct.shadowColor = 'rgb(221, 163, 121)';
+    ct.shadowBlur = 16;
+    ct.shadowOffsetX = 0;
+    ct.shadowOffsetY = 0;
+    ct.strokeStyle = 'rgb(255, 209, 176)';
+    ct.stroke();
+    ct.fill();
 }
 
 function refresh() {
-    drawGlyph();
+    ctg.clearRect(0, 0, 512, 512);
+    drawGlyph(canvasg, currentGlyph);
     mix();
 }
     
@@ -266,7 +338,7 @@ function changeBgColor() {
     bgcolor[1] = parseInt(t.substr(3, 2), 16);
     bgcolor[2] = parseInt(t.substr(5, 2), 16);
     
-    drawBackground();
+    drawBackground(canvasbg, bgcolor);
     mix();
 }
 
@@ -277,7 +349,8 @@ function changeGlyph() {
         localStorage.setItem('currentGlyph', currentGlyph);
     }
     
-    drawGlyph();
+    ctg.clearRect(0, 0, 512, 512);
+    drawGlyph(canvasg, currentGlyph);
     mix();
 }
 
