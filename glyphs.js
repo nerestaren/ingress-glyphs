e500367852586e78a9623ee6e5d9a8444c663d4c
Antoni Oliver <antoni.oliver.tomas@gmail.com>
2015-07-04 02:26:06 +0200

    map + localstorage

---
+++ glyphs.js
@@ -0,0 +1,23 @@
+var glyphs = {
+	// A
+	avoid: [5, 10, 1, 9, 2],	
+	// C
+	capture: [7, 6, 0, 2, 9],
+	// D
+	danger: [10, 4, 0, 7],
+	// E
+    enlightened_enlightenment: [4, 1, 0, 4, 10, 9, 8, 7],
+	// F
+	future: [9, 1, 2, 8],
+	// G
+	gain: [5, 4],
+	// L
+	liberate: [6, 4, 0, 9, 10],
+	// P
+    past: [5, 4, 3, 6],
+    present: [4, 3, 2, 1],
+	// R
+    resistance: [3, 7, 0, 10, 4, 1],
+	// X
+	xm: [4, 3, 0, 2, 1, 4]
+};
